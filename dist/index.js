25fe297e9a00fc1f0935639e1383a81ef684a164
Vasanth Developer <vsnthdev@gmail.com>
2021-10-18 14:15:27 +0530

    🔨 | created itivrutaha logger

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,5 @@
  *  Entry executable file for nirvahaka's CLI app.
  *  Created On 18 October 2021
  */
-
-import logger from './logger/index.js'
-
-await logger()
+import logger from './logger/index.js';
+await logger();
---
+++ src/logger/index.ts
@@ -0,0 +1,32 @@
+/*
+ *  This file configures itivrutaha.
+ *  Created On 18 October 2021
+ */
+
+import chalk from 'chalk';
+import itivrutaha from 'itivrutaha';
+
+export let logger 
+
+const banner = `   ${chalk.redBright('_ __')}    \n ${chalk.redBright(
+    ` | '_ \\`,
+)}   🔬 ${chalk.bold.whiteBright(
+    'nirvahaka',
+)} is a CLI application 🚀\n  ${chalk.redBright(
+    '| | | |',
+)}  for structurally 📡 creating, managing\n  ${chalk.redBright(
+    '|_| |_|',
+)}  and storing 📹 video projects.\n`
+
+export default async () => {
+    console.log(banner)
+
+    logger = await itivrutaha.createNewLogger({
+        appName: 'nirvahaka',
+        bootLog: false,
+        shutdownLog: false,
+        theme: {
+            string: ':emoji :type :message',
+        }
+    })
+}
