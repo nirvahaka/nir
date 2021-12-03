fa075aea0673eafa0f0f44c94a1ed0577cde71fe
Vasanth Developer <vsnthdev@gmail.com>
2021-12-03 21:51:31 +0530

    🔨 renamed volumes/status to volumes/_root

--- dist/cmds/volumes/index.js
+++ dist/cmds/volumes/index.js
@@ -4,14 +4,14 @@
  */
 import { Command } from 'commander';
 import add from './add/cmd/index.js';
-import status from './status/cmd/index.js';
+import root from './_root/cmd/index.js';
 import open from './open/cmd/index.js';
 import remove from './remove/cmd/index.js';
 export default new Command()
     .name('volumes')
     .description('outputs status info of one or more volumes')
     .option('--no-pretty', 'print a pretty table instead of JSON', true)
-    .action(status)
+    .action(root)
     .addCommand(add)
     .addCommand(open)
     .addCommand(remove);
--- src/cmds/volumes/index.ts
+++ src/cmds/volumes/index.ts
@@ -5,7 +5,7 @@
 
 import { Command } from 'commander'
 import add from './add/cmd/index.js';
-import status from './status/cmd/index.js';
+import root from './_root/cmd/index.js';
 import open from './open/cmd/index.js';
 import remove from './remove/cmd/index.js';
 
@@ -13,7 +13,7 @@ export default new Command()
     .name('volumes')
     .description('outputs status info of one or more volumes')
     .option('--no-pretty', 'print a pretty table instead of JSON', true)
-    .action(status)
+    .action(root)
     .addCommand(add)
     .addCommand(open)
     .addCommand(remove)
