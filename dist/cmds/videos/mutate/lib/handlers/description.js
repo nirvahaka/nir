59873bceb84ec6306b066131f6879a394c4f8c07
Vasanth Developer <vsnthdev@gmail.com>
2021-12-15 00:14:37 +0530

    ✨ added description mutation helper

---
+++ dist/cmds/videos/mutate/lib/handlers/description.js
@@ -0,0 +1,19 @@
+/*
+ *  Updates the description of a given video if changed.
+ *  Created On 15 December 2021
+ */
+import merge from 'deepmerge';
+import { db } from '~database/index.js';
+export default async ({ description }, changed, original) => {
+    description = merge(original.description, description);
+    await db.video.update({
+        data: {
+            description: JSON.stringify(description),
+        },
+        where: {
+            slug: original.slug,
+        },
+    });
+    // todo: update the description on YouTube
+    // as well if reflect has been given!
+};
