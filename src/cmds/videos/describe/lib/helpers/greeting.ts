8ebed8e6c2f0c9181945ede1bd56fd1b4b7ffd17
Vasanth Developer <vsnthdev@gmail.com>
2022-03-25 19:59:50 +0530

    ✨ created greeting hbs helper

---
+++ src/cmds/videos/describe/lib/helpers/greeting.ts
@@ -0,0 +1,14 @@
+/*
+ *  Returns a random string of gratitude greeting everything invoked.
+ *  Created On 25 March 2022
+ */
+
+const greetings = [
+    '😊 Thank you for watching!',
+    '👏 Have an awesome day!',
+    'Keep rocking 🤘',
+    'Keep knowing, keep growing 🤓',
+]
+
+export default async () =>
+    greetings[Math.floor(Math.random() * greetings.length)]
