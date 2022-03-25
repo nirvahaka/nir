47677e004a0595c3b0e6d2c7769af7dccf0edc39
Vasanth Developer <vsnthdev@gmail.com>
2022-03-26 00:39:25 +0530

    ✨ created playlist hbs helper

---
+++ src/cmds/videos/describe/lib/helpers/playlist.ts
@@ -0,0 +1,19 @@
+/*
+ *  Returns a playlist block with the video block combined.
+ *  Created On 26 March 2022
+ */
+
+import getYouTube from '~vendor/youtube.js'
+
+export default async id => {
+    // get YouTube's authenticated API client
+    const { auth, youtube } = await getYouTube()
+
+    const { data } = await youtube.playlists.list({
+        id,
+        auth,
+        part: ['snippet'],
+    })
+
+    return `${data.items[0].snippet.title}\nhttps://youtube.com/playlist?list=${id}`
+}
