30a7e1f376938a1c1c90f31be3659179cbf6afad
Vasanth Developer <vsnthdev@gmail.com>
2022-03-25 21:47:13 +0530

    ✨ created article hbs helper

---
+++ src/cmds/videos/describe/lib/helpers/article.ts
@@ -0,0 +1,21 @@
+/*
+ *  Outputs a custom article provided it's link.
+ *  Created On 25 March 2022
+ */
+
+import axios from 'axios'
+import metascraper from 'metascraper'
+import title from 'metascraper-title'
+import safeLink from './link'
+
+export default async link => {
+    const { data } = await axios({
+        method: 'GET',
+        url: link,
+    })
+
+    const getMeta = metascraper([title()])
+    const meta = await getMeta({ url: link, html: data })
+
+    return `${meta.title}\n${safeLink(link)}`
+}
