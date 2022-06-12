0ff0f0967a765c72c9982e19fe6eec6cc292d85c
Vasanth Developer <vsnthdev@gmail.com>
2022-06-12 19:26:53 +0530

    ✨ added timestamps helper to description

---
+++ src/cmds/videos/describe/lib/helpers/timestamps.ts
@@ -0,0 +1,19 @@
+/*
+ *  Outputs YouTube compatible timestamps in the description.
+ *  Created On 12 June 2022
+ */
+
+export default async stamps => {
+    // handle when there are no timestamps
+    if (Object.keys(stamps).length == 0) return ''
+
+    if (Object.keys(stamps)[0] != '00:00') {
+        stamps = Object.assign({ '00:00': 'Introduction' }, stamps)
+    }
+
+    const returnable = []
+
+    for (const time in stamps) returnable.push(`${time}   ${stamps[time]}`)
+
+    return returnable.join('\n')
+}
