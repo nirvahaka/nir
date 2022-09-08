af0759076b1471f226b12ef002b826a023539e46
Vasanth Developer <vsnthdev@gmail.com>
2022-09-08 20:25:05 +0530

    🐛 publish time doesn't get recorded when mutating youtube

--- src/cmds/videos/mutate/lib/handlers/youtube.ts
+++ src/cmds/videos/mutate/lib/handlers/youtube.ts
@@ -3,13 +3,33 @@
  *  Created On 12 December 2021
  */
 
+import { DateTime } from 'luxon'
 import { db } from '~database/index.js'
+import getYouTube from '~vendor/youtube.js'
 
-export default async ({ youtube }, changed, original) => {
+export default async ({ youtube: id }, changed, original) => {
+    // get YouTube's authenticated API client
+    const { auth, youtube } = await getYouTube()
+
+    let published = null
+
+    if (id != null) {
+        const meta = await youtube.videos.list({
+            id,
+            auth,
+            part: ['snippet', 'status', 'liveStreamingDetails'],
+        })
+    
+        // meta.data.items[0].snippet.publishedAt
+        published = meta.data.items[0].status.publishAt
+            || meta.data.items[0].snippet.publishedAt
+    }
+
     await db.video.update({
         data: {
-            youtube,
-            status: youtube == null ? 'active' : 'published',
+            youtube: id,
+            status: id == null ? 'active' : 'published',
+            published: id == null ? null : DateTime.fromISO(published).toJSDate(),
         },
         where: {
             slug: original.slug,
