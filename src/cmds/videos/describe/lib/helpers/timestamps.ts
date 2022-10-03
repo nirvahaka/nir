422211df538716db87812c9fe1e7747165e7f988
Vasanth Developer <vsnthdev@gmail.com>
2022-10-03 14:32:44 +0530

    🐛 fixed timestamps aren't sorted in description

--- src/cmds/videos/describe/lib/helpers/timestamps.ts
+++ src/cmds/videos/describe/lib/helpers/timestamps.ts
@@ -7,13 +7,42 @@ export default async stamps => {
     // handle when there are no timestamps
     if (Object.keys(stamps).length == 0) return ''
 
+    // ensure, all sections of a timestamp contain both digits
+    for (const key in stamps) {
+        // the new key of the object
+        // which ensures that a 0 is in the beginning
+        // if it's only one digit
+        const newKey = key.split(':')
+            .map(sec => sec.length < 2 ? `0${sec}` : sec)
+            .join(':')
+        
+        // capturing old value
+        const oldValue = stamps[key]
+        delete stamps[key]
+
+        stamps[newKey] = oldValue
+    }
+
+    // add introduction if missing
     if (Object.keys(stamps)[0] != '00:00') {
         stamps = Object.assign({ '00:00': 'Introduction' }, stamps)
     }
 
+    // sort them in proper order, so they don't get out of place
+    const sortedStamps = Object.keys(stamps).sort((a: string, b: string) => {
+        if (Number(a.replace(':', '')) < Number(b.replace(':', ''))) {
+            return -1
+        } else if (Number(a.replace(':', '')) > Number(b.replace(':', ''))) {
+            return 1
+        } else {
+            return 0
+        }
+    })
+
     const returnable = []
 
-    for (const time in stamps) returnable.push(`${time}   ${stamps[time]}`)
+    // follow sorted timestamps and populate our returnable
+    for (const time of sortedStamps) returnable.push(`${time}   ${stamps[time]}`)
 
     return returnable.join('\n')
 }
